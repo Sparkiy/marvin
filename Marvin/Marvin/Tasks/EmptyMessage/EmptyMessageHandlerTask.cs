32a2b8b18879db106493a9c511762cca4072925c
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2016-04-27 22:57:03 +0200

    Empty message handler task comments

--- Marvin/Marvin/Tasks/EmptyMessage/EmptyMessageHandlerTask.cs
+++ Marvin/Marvin/Tasks/EmptyMessage/EmptyMessageHandlerTask.cs
@@ -4,9 +4,19 @@ using Microsoft.Bot.Connector;
 
 namespace Marvin.Tasks.EmptyMessage
 {
+    /// <summary>
+    /// The empty message handler.
+    /// </summary>
     public class EmptyMessageHandlerTask : BotMessagePipelineTask
     {
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+        /// <summary>
+        /// Handles the empty message.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <returns>If message has no content, message is returned to user.</returns>
         public override async Task<BotMessage> HandleMessage(BotMessage message)
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
         {
             if (!message.HasContent())
             {
--- Marvin/Marvin/Tasks/EmptyMessage/EmptyMessageHandlerTaskExtensions.cs
+++ Marvin/Marvin/Tasks/EmptyMessage/EmptyMessageHandlerTaskExtensions.cs
@@ -2,8 +2,16 @@ using Marvin.Pipeline;
 
 namespace Marvin.Tasks.EmptyMessage
 {
+    /// <summary>
+    /// The <see cref="EmptyMessageHandlerTask"/> extensions.
+    /// </summary>
     public static class EmptyMessageHandlerTaskExtensions
     {
+        /// <summary>
+        /// Registers the <see cref="EmptyMessageHandlerTask"/> to the <see cref="BotMessagePipeline"/>.
+        /// </summary>
+        /// <param name="pipeline">The bot message pipeline.</param>
+        /// <returns>Returns the <see cref="BotMessagePipeline"/> to enable subsequent calls.</returns>
         public static BotMessagePipeline AddEmptyHandler(this BotMessagePipeline pipeline)
         {
             pipeline.RegisterTask(new EmptyMessageHandlerTask());
